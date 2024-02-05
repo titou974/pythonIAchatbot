545adb98890d2a21862c6c7b206d57ead890c00b
Titouan Hirsch <titouanhirsch@gmail.com>
2024-02-05 23:59:34 +0100

    refacto script

--- script.py
+++ script.py
@@ -10,11 +10,9 @@ from selenium.common.exceptions import NoSuchElementException
 import time
 import re
 
-
-
 conversationNumberLimit = 20
 processed_usernames = set()
-usernameUser = "bichou"
+usernameUser = ""
 conversation_states = {}
 users = {}
 context_message = "Tfk ?"
@@ -22,16 +20,33 @@ greeting = "Salut chou"
 invitation = "Je suis sur mon mym perso, si tu veux on peut continuer à parler un peu dessus, j'échange du contenue sympa. passe à l'occasion :) : https://mym.fans/Petite_creature"
 newMessage = False
 
-
-def init_driver(proxy=None):
+def init_driver():
     options = webdriver.ChromeOptions()
-    if proxy:
-        options.add_argument(f'--proxy-server={proxy}')
+    # if proxy:
+    #     options.add_argument(f'--proxy-server={proxy}')
     return webdriver.Chrome(options=options)
 
+def check_for_initialization(driver, timeout=30):
+    driver.get("https://www.coco.gg/chat/index.html#natasha92_2_29_36213_0_290649056_0_")
+    end_time = time.time() + timeout
+    while True:
+        try:
+            time.sleep(2)
+            searchy_div = driver.find_element(By.ID, 'searchy')
+            # Look for any child divs with class "fixedsy" inside "searchy" div
+            fixedsy_divs = searchy_div.find_elements(By.CLASS_NAME, 'fixedsy')
+            # If any "fixedsy" divs are found, return False
+            if len(fixedsy_divs) != 0:
+                break
+        except TimeoutException:
+            pass 
+        if time.time() > end_time:
+            raise TimeoutException("The element did not initialize within the expected time.")
+        driver.refresh()
+        time.sleep(5)  
+
 def verify_email(driver, email):
-    driver.get("https://www.coco.gg/chat/index.html#lolach_2_23_28517_0_676678502_0_")
-    time.sleep(5)
+    time.sleep(2)
     WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, 'opt4'))).click()
     time.sleep(2)
     WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//div[contains(text(), 'Récupération profil')]"))).click()
@@ -68,35 +83,35 @@ def locate_chat_tab(driver):
         EC.presence_of_element_located((By.ID, 'ongdiv0'))  # Replace with an ID unique to the chat page
     )
 
-def chat_with_user(driver, action, element):
+def chat_with_user(user, driver, action, element):
     input_field = WebDriverWait(driver, 5).until(
         EC.element_to_be_clickable((By.ID, "cocoa"))
     )
-    if usernameUser not in conversation_states:
+    if user not in conversation_states:
         input_field.send_keys(greeting)
         input_field.send_keys(Keys.ENTER)
-        conversation_states[usernameUser] = {'greeting_sent': True, 'messages': [greeting], 'conversation_end': False}
+        conversation_states[user] = {'greeting_sent': True, 'messages': [greeting], 'conversation_end': False}
         time.sleep(2)
-    elif context_message not in conversation_states[usernameUser]['messages']:
+    elif context_message not in conversation_states[user]['messages']:
         input_field.send_keys(context_message)
         input_field.send_keys(Keys.ENTER)
-        conversation_states[usernameUser]['messages'].append(context_message)
+        conversation_states[user]['messages'].append(context_message)
         time.sleep(2)
-    elif invitation not in conversation_states[usernameUser]['messages']: 
+    elif invitation not in conversation_states[user]['messages']: 
         input_field.send_keys(invitation)
         input_field.send_keys(Keys.ENTER)
         time.sleep(2)   
-        conversation_states[usernameUser]['messages'].append(invitation)
-        conversation_states[usernameUser]['conversation_end'] = True
+        conversation_states[user]['messages'].append(invitation)
+        conversation_states[user]['conversation_end'] = True
         action.context_click(element).perform()
-        processed_usernames.add(usernameUser)
-        print(f'La conversation avec {users[usernameUser]["username"]} est terminée')
+        processed_usernames.add(user)
+        print(f'La conversation avec {users[user]["username"]} est terminée')
         time.sleep(2)
     else:
-        processed_usernames.add(usernameUser)
+        processed_usernames.add(user)
         print( 'conversation déjà terminée')
 
-def handle_new_message(usernameUser, driver):
+def handle_new_message(name, driver):
     text_container = driver.find_element(By.ID, "textum")
     html_content = text_container.get_attribute("innerHTML")
 
@@ -105,23 +120,24 @@ def handle_new_message(usernameUser, driver):
     
     for username, message in pairs:
         message = message.strip()
-        if usernameUser not in users:
-            users[usernameUser] = {'username': usernameUser, 'messages': [message], 'newMessage': True}
-        elif message not in users[usernameUser]['messages']:
-            users[usernameUser]['messages'].append(message)
-            users[usernameUser]['newMessage'] = True
+        if name not in users:
+            users[name] = {'username': name, 'messages': [message], 'newMessage': True}
+        elif message not in users[name]['messages']:
+            users[name]['messages'].append(message)
+            users[name]['newMessage'] = True
         else:
             print('message déjà traité')
-            users[usernameUser]['newMessage'] = False
+            users[name]['newMessage'] = False
 
 def main():
     driver = init_driver()
     action = ActionChains(driver)
     try:
+        check_for_initialization(driver)
         verify_email(driver, 'lafermelescollegiens@gmail.com')
-        login(driver, 'lolamym', '23', 75002)
+        login(driver, 'natasha92', '29', 91000)
         switch_to_new_tab(driver)
-        locate_chat_tab(driver)
+        check_for_initialization(driver)
         while True:
             i = 1
             while i < conversationNumberLimit:  
@@ -141,12 +157,13 @@ def main():
                         handle_new_message(usernameUser, driver)
                         time.sleep(5)
                         if users[usernameUser]['newMessage'] == True:
-                            chat_with_user(driver, action, element)
+                            chat_with_user(usernameUser, driver, action, element)
+                        print('conversationsIA', conversation_states)
+                        print('users', users)
 
                     else:
                         print(f'Conversation for username {usernameUser} already processed or ended.')
                         action.context_click(element).perform()
-                    print('conversationsIA', conversation_states)
                     i += 1
 
                 except TimeoutException:
@@ -158,16 +175,6 @@ def main():
                     break 
                 except Exception as e:
                         print(f"An error occurred: {e}")
-                        if usernameUser not in processed_usernames:
-                            handle_new_message(usernameUser, driver)
-                            time.sleep(5)
-                            if users[usernameUser]['newMessage'] == True:
-                                chat_with_user(driver, action, element)
-
-                        else:
-                            print(f'Conversation for username {usernameUser} already processed or ended.')
-                            action.context_click(element).perform()
-                        print('conversationsIA', conversation_states)
                         i += 1
 
             time.sleep(5) 
